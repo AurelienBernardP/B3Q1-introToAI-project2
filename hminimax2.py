8b541d15a4b2cafcc239841c3ba472c1db500168
YasQueen <k.ozdemir@student.uliege.be>
2019-11-11 19:43:55 +0100

    hminimax2.py is final. Coding style & pep8

--- hminimax2.py
+++ hminimax2.py
@@ -1,12 +1,104 @@
-# Complete this class for all parts of the project
-import time
-import math
 from pacman_module.util import *
 from pacman_module.game import Agent
 from pacman_module.pacman import Directions
+import math
+
 
 def key(state):
-    return (state.getPacmanPosition(), state.getFood(), state.getGhostPosition(1),state.getGhostDirection(1))
+    """
+    Returns a key that uniquely identifies a Pacman game state.
+
+    Arguments:
+    ----------
+    - `state`: the current game state. See FAQ and class
+               `pacman.GameState`.
+
+    Return:
+    -------
+    - A hashable key object that uniquely identifies a Pacman game state.
+    """
+    return (state.getPacmanPosition(), state.getFood(),
+            state.getGhostPosition(1), state.getGhostDirection(1))
+
+
+def getNbFood(state):
+    """
+    Returns the number of food in the given Pacman game state.
+
+    Arguments:
+    ----------
+    - `state`: the current game state. See class
+               `pacman.GameState`.
+
+    Return:
+    -------
+    - An integer representing the number of food in the given state.
+    """
+    foodMatrix = state.getFood()
+    nbFood = 0
+    # Going through the matrix to count the remaining food in the game
+    for i in range(foodMatrix.width):
+        for j in range(foodMatrix.height):
+            if foodMatrix[i][j] is True:
+                nbFood += 1
+    return nbFood
+
+
+def cutOff(state, depth, depthMax):
+    """
+    Indicates if the state should expand or not.
+
+    Arguments:
+    ----------
+    - `state`: the current game state. See class `pacman.GameState`.
+    - `depth`: the depth of 'state' compared to the initial state
+    - `depthMax`: the maximal depth to visit based on the initial state
+
+    Return:
+    -------
+    - A boolean which is true if the state should stop expanding.
+    """
+    if state.isWin() or state.isLose() or depth > depthMax:
+        return True
+    else:
+        return False
+
+
+def evals(state):
+    """
+    Returns an estimation of the utility function for the given state
+
+    Arguments:
+    ----------
+    - `state`: the current game state. See class `pacman.GameState`.
+
+    Return:
+    -------
+    - A real number which represents the estimation of the utility
+    function for the given state.
+    """
+    if (state.isWin() or state.isLose()):
+        return state.getScore()
+
+    # Manhattan distance between Pacman and Ghost
+    pacmanPosition = state.getPacmanPosition()
+    ghostPosition = state.getGhostPosition(1)
+    distGhostPacman = manhattanDistance(pacmanPosition, ghostPosition)
+
+    # Manhattan distance between Pacman and the closest food
+    score = state.getScore()
+    gameGrid = state.getFood()
+    minDistFood = math.inf
+    nbFood = 0
+    for i in range(gameGrid.width):
+        for j in range(gameGrid.height):
+            if gameGrid[i][j] is True:
+                nbFood += 1
+                manhattanDist = manhattanDistance((i, j), pacmanPosition)
+                if manhattanDist < minDistFood:
+                    minDistFood = manhattanDist
+
+    return 3*score + 2*distGhostPacman - 5*minDistFood - 40*nbFood
 
 
 class PacmanAgent(Agent):
@@ -17,54 +109,6 @@ class PacmanAgent(Agent):
         - `args`: Namespace of arguments from command-line prompt.
         """
         self.move = None
-    def getNbFood(self,state):
-        foodMatrix = state.getFood()
-        nbFood = 0
-
-        """Going through the matrix to count the remaining food in the game"""
-        for i in range(foodMatrix.width):
-            for j in range(foodMatrix.height):
-                if foodMatrix[i][j] is True:
-                    nbFood += 1
-        
-        return nbFood
-
-    def cut_off(self, state, depth, maxdepth):
-        # Terminal State
-        if (state.isWin() or state.isLose()):
-            return True
-
-        # Expansion if food eaten
-        if(depth > maxdepth):
-            return True
-        else:
-            return False
-
-
-    def evals(self, state):
-        # Terminal State
-        if (state.isWin() or state.isLose()):
-            return state.getScore()
-        score = state.getScore()
-        gameGrid = state.getFood()
-
-        pacmanPosition = state.getPacmanPosition()
-        ghostPosition = state.getGhostPosition(1)
-        ghostDistance = manhattanDistance(pacmanPosition, ghostPosition)
-
-
-        minDistFood = math.inf
-        nbFood = 0
-        """Going through the matrix to count the remaining food in the game"""
-        for i in range(gameGrid.width):
-            for j in range(gameGrid.height):
-                if (gameGrid[i][j] is True): 
-                    nbFood +=1
-                    if (manhattanDistance((i,j), pacmanPosition )) < minDistFood:
-                        minDistFood =  (manhattanDistance((i,j), pacmanPosition ))
-
-        return 3*score + 2*ghostDistance - 5 * minDistFood - 40 * nbFood
-
 
     def get_action(self, state):
         """
@@ -80,38 +124,39 @@ class PacmanAgent(Agent):
         - A legal move as defined in `game.Directions`.
         """
 
-        self.initNbFood = self.getNbFood(state) + 1
+        self.initNbFood = getNbFood(state) + 1
         self.depthExpansion = 2
 
         try:
-            self.hminimax(state, 0, 0, (self.initNbFood - self.getNbFood(state))*self.depthExpansion)
+            nbFoodLeft = self.initNbFood - getNbFood(state)
+            self.hminimax(state, 0, 0, nbFoodLeft*self.depthExpansion)
             return self.move
 
         except IndexError:
             return Directions.STOP
 
     def hminimax(self, state, agent, depth, depthMax):
-
-        #Cas de base
-        if self.cut_off(state, depth, depthMax) :
-            return self.evals(state)
-
+        if cutOff(state, depth, depthMax):
+            return evals(state)
         max = -math.inf
         min = math.inf
-        # pacman
-        if(agent == 0) :
-            for succ_state, succ_move in state.generatePacmanSuccessors():
-                value = self.hminimax(succ_state, 1, depth + 1, (self.initNbFood - self.getNbFood(state))*self.depthExpansion)
-                if value > max :
-                    max = value
-                    if(depth == 0):
-                        self.move = succ_move
-            return max
+        nbFoodLeft = self.initNbFood - getNbFood(state)
 
-        else :
-            for succ_state, succ_move in state.generateGhostSuccessors(1):
-                value = self.hminimax(succ_state, 0, depth+1, (self.initNbFood - self.getNbFood(state))*self.depthExpansion)
-                if value < min :
+        # Pacman has the move
+        if agent == 0:
+            for succState, succMove in state.generatePacmanSuccessors():
+                value = self.hminimax(succState, 1, depth+1,
+                                      nbFoodLeft*self.depthExpansion)
+                if value > max:
+                    max = value
+                    if depth == 0:
+                        self.move = succMove
+            return max
+        # Ghost has the move
+        else:
+            for succState, succMove in state.generateGhostSuccessors(1):
+                value = self.hminimax(succState, 0, depth+1,
+                                      nbFoodLeft*self.depthExpansion)
+                if value < min:
                     min = value
             return min
-    
